8dca6d86599daaba1c18fab58bd2c681e905f31a
James Geneser <jamesgeneser@gmail.com>
2023-07-19 14:50:33 -0600

    first commit cloned over colorado birds

---
+++ src/bird-cards/American-Crow.js
@@ -0,0 +1,127 @@
+import React from "react";
+import { Card, Image } from "react-bootstrap";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { Row, Col } from "react-bootstrap";
+
+export default function AmericanCrow() {
+  const [showRange, setShowRange] = useState(false);
+  const [showDescription, setShowDescription] = useState(false);
+  const [showCall, setShowCall] = useState(false);
+
+  const handleCloseRange = () => setShowRange(false);
+  const handleCloseDescription = () => setShowDescription(false);
+  const handleCloseCall = () => setShowCall(false);
+  const handleShowRange = () => setShowRange(true);
+  const handleShowDescription = () => setShowDescription(true);
+  const handleShowCall = () => setShowCall(true);
+
+  return (
+    <>
+      <Card className="card m-2">
+        <Card.Header className="bird-name bg-none">American Crow</Card.Header>
+        <Card.Img src={require("../media/images/birds/americanCrow.jpeg")} />
+
+        <Card.Footer className="button-row ">
+          <Row className="">
+            <Col xs={4} lg={4} className="text-center">
+              <Button
+                className="button "
+                variant="secondary"
+                onClick={handleShowRange}
+              >
+                Range
+              </Button>
+            </Col>
+            <Col xs={4} lg={4} className="text-center">
+              <Button
+                className="button "
+                variant="secondary"
+                onClick={handleShowDescription}
+              >
+                Description
+              </Button>
+            </Col>
+            <Col xs={4} lg={4} className="text-center">
+              <Button
+                className="button"
+                variant="secondary"
+                onClick={handleShowCall}
+              >
+                Call
+              </Button>
+            </Col>
+          </Row>
+        </Card.Footer>
+      </Card>
+
+      <Modal show={showRange} onHide={handleCloseRange}>
+        <Modal.Header closeButton>
+          <Modal.Title>Range Map</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Image
+            className="img-fluid"
+            src={require("../media/images/maps/americanCrowMap.jpeg")}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Image
+            className="img-fluid"
+            src={require("../media/images/maps/birdMapKey.png")}
+          />
+          <Button variant="secondary" onClick={handleCloseRange}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={showDescription} onHide={handleCloseDescription}>
+        <Modal.Header closeButton>
+          <Modal.Title>Description</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            {" "}
+            American Crows are familiar over much of the continent: large,
+            intelligent, all-black birds with hoarse, cawing voices. They are
+            common sights in treetops, fields, and roadsides, and in habitats
+            ranging from open woods and empty beaches to town centers. They
+            usually feed on the ground and eat almost anything—typically
+            earthworms, insects and other small animals, seeds, and fruit; also
+            garbage, carrion, and chicks they rob from nests. Their flight style
+            is unique, a patient, methodical flapping that is rarely broken up
+            with glides.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseDescription}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={showCall} onHide={handleCloseCall}>
+        <Modal.Header closeButton>
+          <Modal.Title>Call</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <iframe
+            className="cornel-bird-call"
+            src="https://macaulaylibrary.org/asset/135405/embed"
+            frameborder="0"
+            allowfullscreen
+          ></iframe>
+        </Modal.Body>
+        <Modal.Footer>
+          {/* <Image
+            className="img-fluid"
+            src={require("../images/birdMapKey.png")}
+          /> */}
+          <Button variant="secondary" onClick={handleCloseCall}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
---
+++ src/components/List.js
@@ -0,0 +1,171 @@
+import React from "react";
+
+import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
+import AmericanCrow from "../bird-cards/American-Crow";
+import { useState } from "react";
+// import Carousel from "react-bootstrap/Carousel";
+
+export default function List() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+  return (
+    <>
+      <Container className="p-0 d-flex flex-column justify-content-center">
+        <AmericanCrow />{" "}
+        {/* <Row className="section-title-row">
+          <h2 className="section-title m-1 text-end">Black Birds</h2>
+        </Row> */}
+        {/* <Row className="m-1 ">
+          <Col className="m-1 p-0">
+            <h2 className="bird-name m-0 p-3">American Crow</h2>{" "}
+            <Carousel
+              activeIndex={index}
+              onSelect={handleSelect}
+              interval={null}
+            >
+              <Carousel.Item interval={null}>
+                <img
+                  className="d-block w-100 carousel-image"
+                  src={require("./images/americanCrow.jpeg")}
+                  alt="First slide"
+                />
+              </Carousel.Item>
+              <Carousel.Item interval={null}>
+                <img
+                  className="d-block w-100 carousel-image "
+                  data-bs-interval="false"
+                  src={require("./images/americanCrowMap.jpeg")}
+                  alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                  <img
+                    className="map-key m-3"
+                    src={require("./images/birdMapKey.png")}
+                  />
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <div className="crow-background">
+                  <p className="bird-description p-2">
+                    {" "}
+                    American Crows are familiar over much of the continent:
+                    large, intelligent, all-black birds with hoarse, cawing
+                    voices. They are common sights in treetops, fields, and
+                    roadsides, and in habitats ranging from open woods and empty
+                    beaches to town centers. They usually feed on the ground and
+                    eat almost anything—typically earthworms, insects and other
+                    small animals, seeds, and fruit; also garbage, carrion, and
+                    chicks they rob from nests. Their flight style is unique, a
+                    patient, methodical flapping that is rarely broken up with
+                    glides.
+                  </p>
+                </div>
+              </Carousel.Item>
+            </Carousel>
+          </Col>
+        </Row>
+        <Row className="m-1 ">
+          <Col className="m-1 p-0">
+            <h2 className="bird-name m-0 p-3">Brewer's Blackbird</h2>{" "}
+            <Carousel
+              activeIndex={index}
+              onSelect={handleSelect}
+              interval={null}
+            >
+              <Carousel.Item interval={null}>
+                <img
+                  className="d-block w-100 carousel-image"
+                  src={require("./images/brewersBlackBirdMale.jpeg")}
+                  alt="First slide"
+                />
+              </Carousel.Item>
+              <Carousel.Item interval={null}>
+                <img
+                  className="d-block w-100 carousel-image "
+                  data-bs-interval="false"
+                  src={require("./images/brewersBlackbirdMap.jpeg")}
+                  alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                  <img
+                    className="map-key m-3"
+                    src={require("./images/birdMapKey.png")}
+                  />
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <div className="crow-background">
+                  <p className="bird-description p-2">
+                    {" "}
+                    American Crows are familiar over much of the continent:
+                    large, intelligent, all-black birds with hoarse, cawing
+                    voices. They are common sights in treetops, fields, and
+                    roadsides, and in habitats ranging from open woods and empty
+                    beaches to town centers. They usually feed on the ground and
+                    eat almost anything—typically earthworms, insects and other
+                    small animals, seeds, and fruit; also garbage, carrion, and
+                    chicks they rob from nests. Their flight style is unique, a
+                    patient, methodical flapping that is rarely broken up with
+                    glides.
+                  </p>
+                </div>
+              </Carousel.Item>
+            </Carousel>
+          </Col>
+        </Row>
+        <Row className="">
+          {" "}
+          <h2 className="bird-name m-0">Brewer's Blackbird</h2>
+          <img
+            src={require("./images/brewersBlackBirdMale.jpeg")}
+            className="bird-image p-0"
+          />
+          <p className="bird-description m-0">bird detailes lorem ipsum</p>
+        </Row>
+        <Row className="">
+          {" "}
+          <h2 className="bird-name m-0">Brown-Headed Cow Bird</h2>
+          <img
+            src={require("./images/brownHeadedCowbirdM.jpeg")}
+            className="bird-image p-0"
+          />
+          <p className="bird-description m-0">bird detailes lorem ipsum</p>
+        </Row>
+        <Row className="">
+          {" "}
+          <h2 className="bird-name m-0">European Starling</h2>
+          <img
+            src={require("./images/europeanStarling.jpeg")}
+            className="bird-image p-0"
+          />
+          <p className="bird-description m-0">bird detailes lorem ipsum</p>
+        </Row>
+        <Row className="">
+          {" "}
+          <h2 className="bird-name m-0">Bullock's Oriole</h2>
+          <img
+            src={require("./images/bullocksOriole.jpeg")}
+            className="bird-image p-0"
+          />
+          <p className="bird-description m-0">bird detailes lorem ipsum</p>
+        </Row>
+        <Row className="">
+          {" "}
+          <h2 className="bird-name m-0">Common Grackle</h2>
+          <img
+            src={require("./images/commonGrackleM.jpeg")}
+            className="bird-image p-0"
+          />
+          <p className="bird-description m-0">bird detailes lorem ipsum</p>
+        </Row> */}
+      </Container>
+    </>
+  );
+}
