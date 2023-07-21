742d7b986807152d731f6db2e33a8d8120de857b
James Geneser <jamesgeneser@gmail.com>
2023-07-21 09:58:27 -0600

    yellowheaded bb added

---
+++ src/bird-cards/Yellow-Headed-Blackbird.js
@@ -0,0 +1,165 @@
+import React from "react";
+import { Card, Image } from "react-bootstrap";
+import { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { Row, Col } from "react-bootstrap";
+
+export default function YellowHeadedBlackbird() {
+  const [showRange, setShowRange] = useState(false);
+  const [showDescription, setShowDescription] = useState(false);
+  const [showCall, setShowCall] = useState(false);
+
+  const handleCloseRange = () => setShowRange(false);
+  const handleCloseDescription = () => setShowDescription(false);
+  const handleCloseCall = () => setShowCall(false);
+  const handleShowRange = () => setShowRange(true);
+  const handleShowDescription = () => setShowDescription(true);
+  const handleShowCall = () => setShowCall(true);
+
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <>
+      <Card className="card m-2">
+        <Card.Header className="bird-name bg-none">
+          Red-Winged Black Bird
+        </Card.Header>
+
+        <Carousel activeIndex={index} onSelect={handleSelect} interval={null}>
+          <Carousel.Item interval={null}>
+            <Card.Img
+              src={require("../media/images/birds/yellowHeadedBlackbirdM.jpeg")}
+            />
+            <Carousel.Caption className="caption">
+              <h3 className="bird-sex">male</h3>
+              {/* <p className="small-bird-title">Brewer's Blackbird</p> */}
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={null}>
+            <Card.Img
+              src={require("../media/images/birds/yellowHeadedBlackbirdF.jpeg")}
+            />
+            <Carousel.Caption className="caption">
+              <h3 className="bird-sex">female</h3>
+              {/* <p className="small-title">Brewer's Blackbird</p> */}
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+
+        <Card.Footer className="button-row ">
+          <Row className="">
+            <Col xs={4} lg={4} className="text-center">
+              <Button
+                className="button "
+                variant="secondary"
+                onClick={handleShowRange}
+              >
+                Range Map
+              </Button>
+            </Col>
+            <Col xs={4} lg={4} className="text-center">
+              <Button
+                className="button "
+                variant="secondary"
+                onClick={handleShowDescription}
+              >
+                Basic Info
+              </Button>
+            </Col>
+            <Col xs={4} lg={4} className="text-center">
+              <Button
+                className="button"
+                variant="secondary"
+                onClick={handleShowCall}
+              >
+                Calls & Songs
+              </Button>
+            </Col>
+          </Row>
+        </Card.Footer>
+      </Card>
+
+      <Modal show={showRange} onHide={handleCloseRange}>
+        <Modal.Header closeButton>
+          <Modal.Title>Range Map</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Image
+            className="img-fluid"
+            src={require("../media/images/maps/yellowHeadedBbMap.jpg")}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Image
+            className="img-fluid"
+            src={require("../media/images/maps/birdMapKey.png")}
+          />
+          <Button variant="secondary" onClick={handleCloseRange}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={showDescription} onHide={handleCloseDescription}>
+        <Modal.Header closeButton>
+          <Modal.Title>Description</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            With a golden head, a white patch on black wings, and a call that
+            sounds like a rusty farm gate opening, the Yellow-headed Blackbird
+            demands your attention. Look for them in western and prairie
+            wetlands, where they nest in reeds directly over the water. They’re
+            just as impressive in winter, when huge flocks seem to roll across
+            farm fields. Each bird gleans seeds from the ground, then leapfrogs
+            over its flock mates to the front edge of the ever-advancing troupe.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseDescription}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={showCall} onHide={handleCloseCall}>
+        <Modal.Header closeButton>
+          <Modal.Title>Call</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <iframe
+            src="https://macaulaylibrary.org/asset/404419/embed"
+            width={250}
+            height={300}
+          ></iframe>
+          <p>
+            Males sing a few musical notes followed by a screeching buzz, rather
+            like a heavy door swinging on a very rusty metal hinge. They sing
+            from cattails, bulrushes, fences, shrubs, or small trees in the
+            morning and evening during the breeding season. Females make a
+            chattering sound that may be considered a song.
+          </p>
+          <p>
+            Their calls include frequent check calls used in many situations
+            during the breeding season, screams given by alarmed females, growls
+            given by fighting or mating males, and harsh rattles given by males
+            when predators are about.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          {/* <Image
+            className="img-fluid"
+            src={require("../images/birdMapKey.png")}
+          /> */}
+          <Button variant="secondary" onClick={handleCloseCall}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
