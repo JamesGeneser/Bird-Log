c44fc6a160918b3ed84ddfb3344b12208dccbf00
James Geneser <jamesgeneser@gmail.com>
2023-08-10 14:05:15 -0600

    cornell lab credited in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,38 @@
 import React from "react";
+import { Navbar } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function Header() {
   return (
     <>
-      <h1 className="m-0 p-3 title">Colorado Birds</h1>
+      <Navbar className="navbar">
+        <Container className="">
+          <Navbar.Brand className="title p-0 ">Colorado Birds</Navbar.Brand>
+
+          <Row>
+            <Col>
+              <p className="citation p-0 m-0">data and images from</p>
+              <Nav.Link
+                className="link"
+                href="https://www.allaboutbirds.org/news/"
+                target="_blank"
+              >
+                Allaboutbirds.org
+              </Nav.Link>
+            </Col>
+            <Col>
+              {" "}
+              <img
+                className="cornellTag p-0 m-0"
+                src={require("../media/images/cornellLab.png")}
+              ></img>
+            </Col>
+          </Row>
+        </Container>
+      </Navbar>
     </>
   );
 }
